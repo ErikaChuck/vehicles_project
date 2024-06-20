dc531982a6aec1ae186fef98b73e7efee9691a93
ErikaChuck <171841312+ErikaChuck@users.noreply.github.com>
2024-06-20 19:54:38 +0000

    agregar ventana de visualización

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ st.header('Venta de vehículos') #encabezado
 st.write('Anuncios de ventas de coches') 
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+data_build = st.checkbox('Visualizar una muestra de los datos')
 hist_build = st.checkbox('Construir histograma') # crear un botón para generar un histograma
 disp_build = st.checkbox('Construir gráfico de dispersión') #crear un botón para generar un gráfico de dispersión
         
+if data_build: #al seleccionar la casilla
+
+            st.write('Visualiza una muestra del conjunto de datos de anuncios de coches')
+
+            print(car_data, sample= 50)
+
 if hist_build: # al hacer clic en el botón
             # escribir un mensaje
             st.write('Construir un histograma para el conjunto de datos de anuncios de venta de coches')
